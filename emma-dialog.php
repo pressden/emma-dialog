8a7c1ea8231935b7ce700ccf8c51fbdd12c3013b
D.S. Webster <dswebs.me@gmail.com>
2022-01-22 23:19:30 -0500

    Add support for global dialogs

--- emma-dialog.php
+++ emma-dialog.php
@@ -26,7 +26,7 @@ function emma_dialog_enqueue_frontend() {
 	$dialog_library_css = plugin_dir_url( __FILE__ ) . 'vendor/dialog-polyfill/dialog-polyfill.css';
 	$dialog_frontend_js = plugin_dir_url( __FILE__ ) . 'js/dialog-frontend.js';
 
-	if( has_block( 'emma/dialog', $post ) ) {
+	if( has_block( 'emma/dialog', $post ) || is_active_sidebar( 'global-dialogs' ) ) {
 		wp_enqueue_script( 'dialog-library', $dialog_library_js, [], EMMA_DIALOG_LIBRARY_VERSION, true );
 		wp_enqueue_style( 'dialog-library', $dialog_library_css, [], EMMA_DIALOG_LIBRARY_VERSION );
 		wp_enqueue_script( 'dialog-frontend', $dialog_frontend_js, ['dialog-library'], EMMA_DIALOG_VERSION, true );
@@ -43,3 +43,35 @@ function emma_dialog_enqueue_editor() {
 	wp_enqueue_script( 'dialog-editor', $dialog_editor_js, ['wp-blocks','wp-editor'], EMMA_DIALOG_VERSION, true );
 }
 add_action( 'enqueue_block_editor_assets', 'emma_dialog_enqueue_editor' );
+
+/**
+ * Add the global dialogs widget area
+ *
+ * @param array $widget_areas Array of widget areas
+ */
+function emma_global_dialogs_widget_area( $widget_areas ) {
+	$widget_areas['global-dialogs'] = array(
+		'name'        => esc_html__( 'Global Dialogs', 'emma' ),
+		'id'          => 'global-dialogs',
+		'description' => esc_html__( 'Global dialogs for use site wide', 'emma' ),
+	);
+
+	return $widget_areas;
+}
+add_filter( 'emma_widget_areas', 'emma_global_dialogs_widget_area' );
+
+/**
+ * Gets the global dialogs template part.
+ */
+function emma_global_dialogs_template() {
+	if ( is_active_sidebar( 'global-dialogs' ) ) {
+		?>
+
+		<aside id="global-dialogs" class="global-dialogs">
+			<?php dynamic_sidebar( 'global-dialogs' ); ?>
+		</aside>
+
+		<?php
+	}
+}
+add_action( 'emma_after', 'emma_global_dialogs_template' );
